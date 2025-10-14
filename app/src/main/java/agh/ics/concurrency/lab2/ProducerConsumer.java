b6c4030deb8b2700145988843c69c2d5562c0abf
JanGawronski <jangawronski04@gmail.com>
2025-10-14 22:31:30 +0200

    Added explanation

--- app/src/main/java/agh/ics/concurrency/lab2/ProducerConsumer.java
+++ app/src/main/java/agh/ics/concurrency/lab2/ProducerConsumer.java
@@ -6,6 +6,17 @@ public class ProducerConsumer {
     public ProducerConsumer() {
     }
 
+    /* why doesnt work
+    p1  notifies nobody var=1 {p1, p2, c}
+    p2  waits                 {p1, c}
+    p1  waits                 {c}
+    c   notifies p2     var=0 {p2, c}
+    c   waits                 {p2}
+    p2  notifies p1     var=1 {p1, p2}
+    p1  waits                 {p2}
+    p2  waits                 {}
+    */
+
     synchronized public void produce() {
         while(available) {
             try {
