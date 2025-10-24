5c9fec7ab150b4230c580426dd9524d5f9306cf3
JanGawronski <jangawronski04@gmail.com>
2025-10-25 00:07:53 +0200

    Implemented multiple buffer producer-consumer using wait()/notify()

--- app/src/main/java/agh/ics/concurrency/lab2/ProducerConsumerMultipleBuffer.java
+++ app/src/main/java/agh/ics/concurrency/lab2/ProducerConsumerMultipleBuffer.java
@@ -1,56 +1,35 @@
 package agh.ics.concurrency.lab2;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class ProducerConsumerMultipleBuffer {
     private int buffer = 0;
     private final int bufferMax;
-    private final ReentrantLock lock = new ReentrantLock();
-    private final Condition notFull = lock.newCondition();
-    private final Condition notEmpty = lock.newCondition();
 
     public ProducerConsumerMultipleBuffer(int bufferMax) {
         this.bufferMax = bufferMax;
     }   
 
-    /* why doesnt work
-    p1  notifies nobody var=1 {p1, p2, c}
-    p2  waits                 {p1, c}
-    p1  waits                 {c}
-    c   notifies p2     var=0 {p2, c}
-    c   waits                 {p2}
-    p2  notifies p1     var=1 {p1, p2}
-    p1  waits                 {p2}
-    p2  waits                 {}
-    */
-
-    public void produce() {
-        lock.lock();
-        try {
-            while (buffer == bufferMax)
-                notFull.await();
-            buffer += (int)(Math.random() * bufferMax / 2) + 1;
-            notEmpty.signal();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();    
-        }    
+    synchronized public void produce() {
+        while(buffer >= bufferMax) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        buffer++;
+        notify();
     }
 
-    public void consume() {
-        lock.lock();
-        try {
-            while (buffer == 0)
-                notEmpty.await();
-            buffer -= (int)(Math.random() * bufferMax / 2) + 1;
-            notFull.signal();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();        
+    synchronized public void consume() {
+        while(buffer <= 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        buffer--;
+        notify();
     }
 
     public void print() {
